3d18e1e13bc05db002f183fe0c143ef12e72690e
Angel Hernandez <angel75@ksu.edu>
2020-02-05 16:28:49 -0600

    <3333

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DoesItBlend
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!\n");
+
+            var b = new Banana();
+            var m = new Mango();
+            var s = new Strawberry();
+            var c = new IceCubes();
+
+            Console.WriteLine(b.Blend());
+            Console.WriteLine(m.Blend());
+            Console.WriteLine(s.Blend() + "\n");
+
+            List<IBlendable> fuits = new List<IBlendable>() {b, m, s, c};
+
+            foreach(var f in fuits)
+            {
+                Console.WriteLine(f.Blend());
+            }
         }
     }
 }
